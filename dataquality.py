8028c4cd4489ef855e1b3ce48d584bdd6a9da5e4
Jonathan Costa <jhoncosta2@gmail.com>
2024-09-26 20:33:30 -0300

    add arquivos para analise

--- dataquality.py
+++ dataquality.py
@@ -7,11 +7,11 @@ class DataQuality:
     def __init__(self, dataset) -> None:
         self.dataset = pd.read_csv(dataset)
         
-    def __repr__(self):
-        return f"""{self.dataset.info()}"""
+    
     
     def descricao(self):
         return self.dataset.describe()
+    
     def colunas(self):
         return self.dataset.index()
     
