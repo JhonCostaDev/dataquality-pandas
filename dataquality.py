544d672b1e0bbfcaf2776d5eb853f411588082fd
Jonathan Costa <jhoncosta2@gmail.com>
2024-09-30 22:23:06 -0300

    final

--- dataquality.py
+++ dataquality.py
@@ -1,17 +1,15 @@
 import numpy as np 
 import pandas as pd
+from IPython.display import display, Markdown
 
 
 #TODO: implementar a escolha do tipo de arquivo no construtor
 
 class DataQuality:
     def __init__(self, dataset) -> None:
-        #self.dataset = pd.read_csv(dataset)
         self.dataset = dataset
         self.dataframe = self.carregar_dataset()
-        #self.delimitador = delimitador <-----APLICAR DELIMITADOR
-        #self.resumo = self.show()
-        #self.categoricas = self.separar_colunas()
+        
         
     def carregar_dataset(self):
         if self.dataset.endswith('.csv'):
@@ -25,26 +23,72 @@ class DataQuality:
     
     ###########################################
     
-    # separar colunas categoricas de colunas numéricas
     
+    def descricao_geral(self):
+        display(Markdown("# Descrição geral do dataset"))
+        linhas, colunas = self.dataframe.shape
+        
+        display("Números de Linhas", linhas)
+        display("Números de Colunas", colunas)
     
+    def exibir_cabecalho(self):
+        display(Markdown("# 5 Primeiras Linhas do Dataset"))
+        display(self.dataframe.head())
+        
+    def exibir_rodape(self):
+        display(Markdown("# 5 Linhas finais do Dataset"))
+        display(self.dataframe.tail())
+        
     
     ###########################################
-    def descricao(self):
-        return self.dataframe.describe()
-    
     def colunas_categoricas(self):
         categoricas = self.dataframe.select_dtypes(exclude='number')
-        return categoricas
+        display(Markdown("# Colunas categóricas do dataset"))
+        display(categoricas)
+        
+    def descricao_categoricas(self):
+        for coluna in self.dataframe:
+            resumo = self.dataframe[coluna].value_counts().reset_index()
+            display(coluna)
+            display(resumo)
+            display('--------------------------')
     
     def colunas_numericas(self):
         numericas = self.dataframe.select_dtypes(include='number')
-        return numericas
+        display(Markdown("# Colunas numéricas do dataset"))
+        display(numericas)
+        
+    def descricao_numericas(self):
+        display(Markdown("# Detalhamento das colunas  numéricas do dataset"))
+        numericas = numericas = self.dataframe.select_dtypes(include='number')
+        display(numericas.describe())
     
     def nulos_por_coluna(self):
-        return self.dataframe.isnull().sum()
+        
+        display(Markdown("# Quantidade de dados nulos ou faltantes."))
+        display(self.dataframe.isnull().sum().reset_index())
+        
+    def dados_duplicados(self):
+        display(Markdown("## Dados Duplicados no Dataset"))
+        saida = f'{self.dataframe.duplicated().sum()}'
+        display(saida)
     
-    ######
+    def valores_unicos(self):
+        display(Markdown("## Valores Únicos no Dataset"))
+        saida = f'{self.dataframe.nunique().sum()}'
+        display(saida)
+    
+    def show(self): # essa função exibirá o resumo do 
+        self.descricao_geral()
+        self.exibir_cabecalho()
+        self.exibir_rodape()
+        self.colunas_categoricas()
+        self.colunas_numericas()
+        self.nulos_por_coluna()
+        self.descricao_numericas()
+        self.valores_unicos()
+        self.dados_duplicados()
+    
     
     def __repr__(self):
         return f"""
@@ -55,18 +99,9 @@ class DataQuality:
     Nº de linhas: {self.dataframe.shape[0]}
     Nº de colunas: {self.dataframe.shape[1]}
     Total valores únicos: {self.dataframe.nunique().sum()}
-    Total de Valores nulos: {self.dataframe.isnull().sum().sum()}
+    Total de valores faltando ou nulos: {self.dataframe.isnull().sum().sum()}
     
-#################################
-    DADOS NULOS por coluna:
-{self.nulos_por_coluna()}
 
-
-##################################
-    RESUMO DAS COLUNAS NUMÉRICAS
-{self.colunas_numericas().describe()}
 """
     
-    def show(self): # essa função exibirá o resumo do dataset
-        pass
         
